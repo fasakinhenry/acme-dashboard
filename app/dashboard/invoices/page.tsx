3ef2b1b7b372090d342542f675f39ec1c8a1b635
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-18 22:42:18 +0100

    feat: replace title in invoice page metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -9,7 +9,7 @@ import { fetchInvoicesPages } from '@/app/lib/data';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Invoices | Acme Dashboard',
+  title: 'Invoices',
 };
 
 export default async function Page(props: {
@@ -32,7 +32,7 @@ export default async function Page(props: {
         <Search placeholder='Search invoices...' />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className='mt-5 flex w-full justify-center'>
