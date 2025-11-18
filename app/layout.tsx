3eec5cde89b40727a80a79c2cdc8a71a29f6b4b5
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-18 22:41:32 +0100

    feat: update page metadata to include a template

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,18 +3,22 @@ import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Acme Dashboard',
-  description: 'The official Next.js Course Dashboard, built with App Router by Fasakin Henry.',
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description:
+    'The official Next.js Course Dashboard, built with App Router by Fasakin Henry.',
   metadataBase: new URL('https://acme-dashboard-saas.vercel.app'),
-}
- 
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
